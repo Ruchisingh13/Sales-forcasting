e20bfce6921dd1fa1cfb496f6785b7250f1b5572
Ruchi Singh <141247708+Ruchisingh13@users.noreply.github.com>
2024-01-12 22:27:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+# Import necessary libraries
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import os
+#os.chdir("path/to/your/app")
+
+
+app = Flask(__name__)
+
+model = pickle.load(open(r'C:/Users/HP/Desktop/SFCML/SALES PREDICTION/model.pkl', 'rb'))
+#os.chdir(r"C:/Users/HP/Desktop/SFCML/SALES PREDICTION/model.pkl")
+model_path = os.path.join(os.path.dirname(__file__), 'SALES PREDICTION', 'model.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+
+@app.route('/results', methods=['POST'])
+def results():
+    data = request.get_json(force=True)
+    predict =model.predict([np.array(list(data.values()))])
+    output = predict[0]
+    return jsonify(output)
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
